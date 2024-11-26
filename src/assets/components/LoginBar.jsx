4f6c459b0cebc50e3871188ce3594221318c63b5
Hosssamkoshok <hosmkos12@gmail.com>
2024-11-26 03:31:54 +0200

    New navbar with login info

---
+++ src/assets/components/LoginBar.jsx
@@ -0,0 +1,19 @@
+import styles from '../styles/LoginBar.module.css'
+import {Link} from 'react-router-dom'
+
+function LoginBar(){
+    return (  
+        <div className={styles.LoginBarGroup}>    
+            
+            <div className={styles.usernameBar}>
+                <img src="../images/Avatar.png" alt="Avtar"className={styles.avatarImage}/ >
+                <Link to="/login" htmlFor="Username" className={styles.userLabel}>Login</Link>
+            </div>
+
+        </div>
+    );
+}
+ 
+export default LoginBar;
+   
+
---
+++ src/assets/components/MainNav.jsx
@@ -0,0 +1,31 @@
+import styles from '../styles/MainNav.module.css'
+import LoginBar from './LoginBar';
+function MainNav(){
+  const menu = ["Booking", "Tourism", "FAQ", "About"];
+  
+  
+  return(
+    <header>
+      <nav>
+        <div className={styles.logodiv}>
+          <img src='../images/logo.png' alt="logo" className={styles.logo}/>
+        </div>
+
+        <div className={styles.navdiv}>
+        {menu.map((element) => (
+          <ul key={element}>
+            <li>
+              <a href="http://">{element}</a>
+            </li>
+          </ul>
+        ))}
+        </div>
+        
+        <div className={styles.logindiv}>
+        <LoginBar/>
+        </div>
+      </nav>
+    </header>
+  );
+}
+export default MainNav;
